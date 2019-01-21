4e6594e225a0667eb1f0acecf434d98a2eb736d7
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 18:22:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,15 @@
 # Add  code here!
  def prime?(n)
    x = 2 
-   until x > n 
-     if n % x == 0 
-       return false 
-     end 
-     x+= 1
-   end
+   if n < 0 
+     return false
+  else 
+      
+     until x > n 
+       if n % x == 0 
+         return false 
+       end 
+       x+= 1
+     end
  end
  
