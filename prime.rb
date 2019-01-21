e419d8889f4b9f5f85d3d4d57a113a0f6d889100
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 18:35:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 # Add  code here!
 def prime?(n)
+  while n >1
     for x in 2..(n-1)
       if n % x == 0
         return false 
       end
     end
     return true
+  end
+  return false
 end
  
